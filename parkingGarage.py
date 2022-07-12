38d1eb96a13fd6196b728e045464d818f26a328f
Brandt Carlson <brandt.carlson11@gmail.com>
2022-07-12 16:15:33 -0500

    working

--- parkingGarage.py
+++ parkingGarage.py
@@ -10,68 +10,68 @@ class parkingGarage():
     def __init__(self):
         self.tickets = 5
         self.parkingSpaces = [
-            {0: 'open', 'paid' : False}, 
             {1: 'open', 'paid' : False}, 
             {2: 'open', 'paid' : False}, 
             {3: 'open', 'paid' : False}, 
-            {4: 'open', 'paid' : False}
+            {4: 'open', 'paid' : False}, 
+            {5: 'open', 'paid' : False}
         ]
         
         #just throwing this in to maybe have some fun with a print statement later; "Look at all the money our parking garage has made!"
         self.bank = 0
         
     def serveSpace(self):
-        for x in range(len(self.parkingSpaces)):
-            if self.parkingSpaces[x][x] == 'open':
+        for x in range(1, 6):
+            if self.parkingSpaces[x-1][x] == 'open':
                 return x
-            elif self.parkingSpaces[x][x] == 'full':
+            elif self.parkingSpaces[x-1][x] == 'full':
                 continue
-            return False
+            
 
     def takeTicket(self):
         a = self.serveSpace()
         if a:
-           self.tickets -= 1
-           self.parkingSpaces[a][a] = 'full'
-           print(f'Please proceed to spot {a}.  This is your ticket, don\'t forget {a} or you\'re stuck here forever. . . ')
+            self.tickets -= 1
+            self.parkingSpaces[a-1][a] = 'full'
+            print(f'Please proceed to spot {a}.  This is your ticket, don\'t forget {a} or you\'re stuck here forever. . . ')
         else:
             print("We're full!  Sorry, better keep looking for a spot to park.")
     
 
 
     def payForParking(self):
-        pay = input('Please enter your ticket #.')
-        z = input("Enter an amount of $35 foo! ")
-        if z == '35':
-            self.parkingSpaces[pay]['paid'] = True
+        pay = int(input('Please enter your ticket #.'))
+        z = int(input("Enter an amount of $35 foo! "))
+        if z == 35:
+            self.parkingSpaces[pay-1]['paid'] = True
             self.bank += z
         else:
-            oth = input("Let's try this again, gimme what you got.")
-            self.parkingSpaces[pay]['paid'] = True
+            oth = int(input("Let's try this again, gimme what you got."))
+            self.parkingSpaces[pay-1]['paid'] = True
             self.bank += oth
         print("Thank you for paying. You get 15mins tops.  Get outta here! ")
 
 
 
     def leaveGarage(self):
-        tic = input("What's your ticket number?")
+        tic = int(input("What's your ticket number?"))
         # if paid- we let them loose and reset
-        if self.parkingSpaces[tic]['paid'] == True:
-            self.parkingSpaces[tic]['paid'] = False
-            self.parkingSpaces[tic][tic] = 'open'
+        if self.parkingSpaces[tic - 1]['paid'] == True:
+            self.parkingSpaces[tic - 1]['paid'] = False
+            self.parkingSpaces[tic - 1][tic] = 'open'
             print('Thank You, Have a wonderful life!')
         # Else we're gonna get that money
         elif self.currentTicket == False:
-            x = input('Gimme all your money, how much you got???')
+            x = int(input('Gimme all your money, how much you got???'))
             if isinstance(x, int):
-                self.parkingSpaces[tic][tic] = 'open'
+                self.parkingSpaces[tic - 1][tic] = 'open'
                 self.tickets += 1
                 self.bank += x
                 print('That works, have a great day!')
             else:
                 # Just trying to have some fun with error handling
-                y = input("Don't make me bust a kneecap, give me a number. . . ")
-                self.parkingSpaces[tic][tic] = 'open'
+                y = int(input("Don't make me bust a kneecap, give me a number. . . "))
+                self.parkingSpaces[tic - 1][tic] = 'open'
                 self.tickets += 1
                 self.bank += y
                 print('That works, have a great day!')
@@ -79,18 +79,18 @@ class parkingGarage():
     
     def run(self):
         print("Welcome to Tony's yard/event parking!")
-        menu = input('p to park, pay to pay, l to leave, done to quit')
-        while menu != 'done':
+        while True:
+            menu = input('p to park, pay to pay, l to leave, done to quit')
             if menu == 'p':
-                # The framework is in, finish it off!
-                pass
+                self.takeTicket()
             elif menu == 'pay':
-                # The framework is in, finish it off!
-                pass
+                self.payForParking()
             elif menu == 'l':
-                # The framework is in, finish it off!
-                pass
-        print(f"\n\nHey Tony!  We made ${self.bank} today.")
+                self.leaveGarage()
+            elif menu == 'done':
+                print(f"\n\nHey Tony!  We made ${self.bank} today.")
+                return False
+        
         
         
 t = parkingGarage()
